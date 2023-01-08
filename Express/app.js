ca024e7618d03bae366c0cc2f8937a3fb08a398f
ranggo pato <patoranggo@gmail.com>
2023-01-08 22:36:33 +0700

    handling routing express

--- Express/app.js
+++ Express/app.js
@@ -1,16 +1,18 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send("<div>test</div>");
-  console.log("in the middleware");
+app.use("/", (req, res, next) => {
+  console.log("always run");
   next();
 });
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("another middleware");
+  res.send("<h1>Add Product</h1>");
+});
+app.use("/", (req, res, next) => {
+  console.log("another middleware");
+  res.send("<h1>Hello from Express</h1>");
 });
 
 app.listen(3000);
