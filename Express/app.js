f89261305f4484d4df3680350b47ef838623581e
ranggo pato <patoranggo@gmail.com>
2023-01-08 22:49:34 +0700

    parsing incoming request

--- Express/app.js
+++ Express/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", (req, res, next) => {
   console.log("always run");
@@ -8,7 +11,14 @@ app.use("/", (req, res, next) => {
 });
 app.use("/add-product", (req, res, next) => {
   console.log("another middleware");
-  res.send("<h1>Add Product</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 app.use("/", (req, res, next) => {
   console.log("another middleware");
